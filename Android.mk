58f5a25403793a8ecb180c616cc577d03217c0bb
Boris Belyaev <bbor1974@gmail.com>
2023-10-06 15:07:50 +0400

    small change
    Change-Id: Ia35ab15bd485beff703d11461446f1e34a110208

--- Android.mk
+++ Android.mk
@@ -86,7 +86,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := vendor.qti.hardware.fm@1.0
-LOCAL_MODULE_OWNER := xiaomi
+LOCAL_MODULE_OWNER := jsr
 LOCAL_SRC_FILES := proprietary/lib/vendor.qti.hardware.fm@1.0.so
 LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
